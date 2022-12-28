2fc38ff5b46eb9608d63c41eaf6555e034aa9bdb
khanshorif331 <khanshorif331@gmail.com>
2022-12-29 01:46:29 +0600

    home paage setup done with the reducer and context api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import ProductCard from '../components/ProductCard'
 import { useProducts } from '../context/ProductProvider'
 
 const Home = () => {
-	const { data } = useProducts()
-	console.log(data)
+	const {
+		state: { products },
+	} = useProducts()
+	console.log(products)
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-			<h1>This is home page</h1>
+			{products.map(product => {
+				return (
+					<ProductCard product={product} key={product._id}></ProductCard>
+				)
+			})}
 		</div>
 	)
 }
